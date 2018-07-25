9b0f5a93b82e2174c57ffa1a44c2d05addbedcf8
chris-schmitz <schmitz.chris@gmail.com>
2018-07-25 09:45:11 -0500

    Fits in ultrasonic sensor and neopixel scaffold code

--- prototypes/sensor-and-servo/sensor-and-servo.ino
+++ prototypes/sensor-and-servo/sensor-and-servo.ino
@@ -1,19 +1,93 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
-*/
-
 #include <Servo.h>
+#include <Adafruit_NeoPixel.h>
 
+#define servoPin 2
 #define triggerPin 3
 #define echoPin 4
-const int servoPin = 2;
+#define neopixelPin 5
 
-Servo myservo; // create servo object to control a servo
+Servo myservo;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(8, neopixelPin, NEO_GRB + NEO_KHZ800);
+
+int pos = 0; // variable to store the servo position
+long duration;
+long distance;
+bool triggerClose = false;
+
+void setup()
+{
+    Serial.begin(9600);
+    // * Attach servo to pin
+    myservo.attach(servoPin);
+
+    // * Setup ultrasonic sensor
+    pinMode(triggerPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+
+    // * Set up neopixel strip
+    strip.begin();
+    strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop()
+{
+    // * Get back into a ready state for the ultrasonic sensor
+    digitalWrite(triggerPin, LOW);
+    delayMicroseconds(2);
+
+    // * Send out a pulse from the trigger
+    digitalWrite(triggerPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(triggerPin, LOW);
+
+    // * Look for a reply pulse from the echo pin
+    duration = pulseIn(echoPin, HIGH);
+
+    // * Calculate distance based on the speed of sound
+    // ! note that the ` / 2` is because the pulse duration accounts for the time
+    // ! the pulse is sent out and coming back, to calculate distance we only want
+    // ! HALF of that round trip
+    distance = duration * 0.034 / 2;
+
+    // Serial.print("Duration: ");
+    // Serial.println(duration);
+    Serial.print("Distance: ");
+    Serial.println(distance);
+
+    if (distance < 50)
+    {
+        Serial.println("close");
+        if (triggerClose == false)
+        {
+            Serial.println("set Trigger close true");
+            triggerClose = true;
+            myservo.write(0);
+            colorWipe(strip.Color(255, 0, 0), 5);
+        }
+    }
+    else if (distance >= 50)
+    {
+        Serial.println("far");
+        if (triggerClose == true)
+        {
+            Serial.println("set Trigger close false");
+            triggerClose = false;
+            myservo.write(180);
+            colorWipe(strip.Color(0, 0, 0), 5);
+        }
+    }
+
+    delay(50);
+}
+
+void peek()
+{
+    // myservo.write
+}
+
+void halfOpen() {}
+
+void open() {}
 
 // ! Pulled from github issues comment: https://github.com/sandeepmistry/arduino-nRF5/issues/23#issuecomment-232268164
 // ! Come back later and extract this to it's own file and try to understand what is going on here.
@@ -72,81 +146,48 @@ uint32_t pulseIn(uint32_t pin, uint32_t state, uint32_t timeout)
         return 0;
 }
 
-int pos = 0; // variable to store the servo position
-long duration;
-long distance;
-
-void setup()
+// ! From the adafruit strandtest
+// * Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait)
 {
-    Serial.begin(9600);
-    // * Attach servo to pin
-    myservo.attach(servoPin);
+    uint16_t i, j;
 
-    // * Setup ultrasonic sensor
-    pinMode(triggerPin, OUTPUT);
-    pinMode(echoPin, INPUT);
+    for (j = 0; j < 256 * 5; j++)
+    { // 5 cycles of all colors on wheel
+        for (i = 0; i < strip.numPixels(); i++)
+        {
+            strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+        }
+        strip.show();
+        delay(wait);
+    }
 }
 
-void loop()
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos)
 {
-    // for (pos = 0; pos <= 180; pos += 1)
-    // { // goes from 0 degrees to 180 degrees
-    //     // in steps of 1 degree
-    //     myservo.write(pos); // tell servo to go to position in variable 'pos'
-    //     delay(15);          // waits 15ms for the servo to reach the position
-    // }
-    // for (pos = 180; pos >= 0; pos -= 1)
-    // {                       // goes from 180 degrees to 0 degrees
-    //     myservo.write(pos); // tell servo to go to position in variable 'pos'
-    //     delay(15);          // waits 15ms for the servo to reach the position
-    // }
-    // return;
-
-    // * Get back into a ready state for the ultrasonic sensor
-    digitalWrite(triggerPin, LOW);
-    delayMicroseconds(2);
-
-    // * Send out a pulse from the trigger
-    digitalWrite(triggerPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(triggerPin, LOW);
-
-    // * Look for a reply pulse from the echo pin
-    duration = pulseIn(echoPin, HIGH);
-
-    // * Calculate distance based on the speed of sound
-    // ! note that the ` / 2` is because the pulse duration accounts for the time
-    // ! the pulse is sent out and coming back, to calculate distance we only want
-    // ! HALF of that round trip
-    distance = duration * 0.034 / 2;
-
-    Serial.print("Duration: ");
-    Serial.println(duration);
-    Serial.print("Distance: ");
-    Serial.println(distance);
-
-    if (distance > 50)
+    WheelPos = 255 - WheelPos;
+    if (WheelPos < 85)
     {
-        Serial.println("Writing 0");
-        myservo.write(0);
+        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
     }
-    else if (distance < 500)
+    if (WheelPos < 170)
     {
-        Serial.println("Writing 180");
-        myservo.write(180);
+        WheelPos -= 85;
+        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
     }
+    WheelPos -= 170;
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
 
-    delay(50);
-
-    // for (pos = 0; pos <= 180; pos += 1)
-    // { // goes from 0 degrees to 180 degrees
-    //     // in steps of 1 degree
-    //     myservo.write(pos); // tell servo to go to position in variable 'pos'
-    //     delay(15);          // waits 15ms for the servo to reach the position
-    // }
-    // for (pos = 180; pos >= 0; pos -= 1)
-    // {                       // goes from 180 degrees to 0 degrees
-    //     myservo.write(pos); // tell servo to go to position in variable 'pos'
-    //     delay(15);          // waits 15ms for the servo to reach the position
-    // }
+// * Pulled over from the `strandtest.ino` example
+void colorWipe(uint32_t c, uint8_t wait)
+{
+    for (uint16_t i = 0; i < strip.numPixels(); i++)
+    {
+        strip.setPixelColor(i, c);
+        strip.show();
+        delay(wait);
+    }
 }
